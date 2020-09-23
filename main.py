54169de4f568d9b42496eb3bdf1287c5f761d40d
Yash Karandikar <nerdstep710@gmail.com>
2020-09-23 11:08:29 -0500

    Basic window structure

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+"""
+Spider Island
+"""
+import arcade
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Window constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Spider Island"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+class SpiderIsland(arcade.Window):
+    """
+    Main game class
+    """
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        arcade.start_render()
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    """ Main method """
+    window = SpiderIsland()
+    window.setup()
+    arcade.run()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
